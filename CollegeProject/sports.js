bb29d9d607115be08d6ccbf37447747eb29ff049
Saambell1996 <Saambell1996@gmail.com>
2018-11-09 11:24:12 +0000

    stadium changes/API

--- CollegeProject/sports.js
+++ CollegeProject/sports.js
@@ -1,5 +1,5 @@
 var url =
-  "https://newsapi.org/v2/top-headlines?sources=talksport&apiKey=e7a9e149d64c4a26aa4462358d220721";
+  "https://newsapi.org/v2/everything?q=premierleague&apiKey=e7a9e149d64c4a26aa4462358d220721";
 
 var req = new Request(url);
 fetch(req)
@@ -11,16 +11,23 @@ fetch(req)
     let data = res['articles'];
     console.log("total records: " + data.length)
     let string = '';
+  
 
     var div = document.getElementById('articles');
     
+   
 
       for(let i= 0; i < data.length; i++){
         string += '<div class="item">';
           string += '<div class="article_title">' + data[i]['title'] + '</div>';
           string += '<div class="article_author">' + data[i]['author'] + '</div>';
+          string += '<div class="article_date">' + data[i]['publishedAt'] + '</div>';
           string += '<div class="article_content">' + data[i]['content'] + '</div>';
         string += '</div>';
+
+        
+       
+   
       }
 
     div.innerHTML += string;    
